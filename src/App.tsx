f280a026b5fd869fb489c12e55ede23cd512df7e
khsily <khsilyqwerty@gmail.com>
2022-05-01 19:37:17 +0900

    기본 페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from 'pages';
+import { About, Home, Project } from 'pages';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/' element={<About />} />
+      <Route path='/' element={<Project />} />
     </Routes>
   );
 }
